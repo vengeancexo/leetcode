97b3ba50cf1698dec0cedbdbc43bd40ab5f8d769
vengeancexo <vikyathsusarla@gmail.com>
2026-09-23 18:57:48 +0530

    Create find-the-highest-altitude.cpp
    leetcode 1732

---
+++ LeetCode-75/Prefix-Sum/find-the-highest-altitude.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+        int sum = 0, ans = 0;
+        for(int i = 0; i < gain.size(); i++) {
+            sum += gain[i];
+            if(sum > ans)
+                ans = sum;
+        }
+        return ans;
+    }
+};
