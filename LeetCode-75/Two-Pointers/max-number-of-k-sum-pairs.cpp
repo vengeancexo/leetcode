cea59ba2a1fe47bcb04e5980063f812ba6bfa4f6
vengeancexo <vikyathsusarla@gmail.com>
2026-09-23 13:39:23 +0530

    Create max-number-of-k-sum-pairs.cpp
    leetcode 1679

---
+++ LeetCode-75/Two-Pointers/max-number-of-k-sum-pairs.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maxOperations(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end());
+        int low = 0, high = nums.size() - 1, ans = 0;
+        while(low < high) {
+
+            int sum = nums[low] + nums[high];
+
+            if(sum < k) {
+                low++;
+            }
+            else if(sum > k) {
+                high--;
+            }
+            else {
+                ans++;
+                low++;
+                high--;
+            }
+        }
+        return ans;
+    }
+};
